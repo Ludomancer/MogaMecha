38e01701a1f6b2fe36123adb6f7e8aa847ad6df8
Erdin Kacan <unique.ek@gmail.com>
2014-01-25 10:07:06 +0200

    Networking Proto

---
+++ Awesome-GGJ-2014-Game/Assets/Nidre/Scripts/Networking.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Networking : MonoBehaviour
+{
+    NetworkView nw;
+    // Use this for initialization
+    void Start()
+    {
+        nw = GetComponent<NetworkView>();
+        LaunchServer();
+
+        Network.Connect("10.0.27.129", 80);
+
+        nw.RPC("PrintText", RPCMode.All, "Osman");
+    }
+
+    // Update is called once per frame
+    void LaunchServer()
+    {
+        Network.incomingPassword = "HolyMoly";
+        var useNat = !Network.HavePublicAddress();
+        Network.InitializeServer(32, 80, useNat);
+    }
+
+    [RPC]
+    void PrintText (string test)
+    {
+        Debug.Log(test);
+    }
+}
